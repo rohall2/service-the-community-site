c093e3061ab580291d3e89027186b5aa8eeb87ce
rohall2 <rohall2@yahoo.com>
2025-11-12 18:04:36 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,1 @@
-import "./globals.css";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Service the Community Inc.",
-  description: "Neighbors helping neighbors.",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en"><body>{children}</body></html>
-  );
-}
+import './globals.css'
